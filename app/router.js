a186b123c0d2d8d3fddc88f1a1ac824b33101b8a
Isaac <hawkins.isaac@gmail.com>
2018-11-01 15:52:02 -0500

    set up basic routes and templates

--- app/router.js
+++ app/router.js
@@ -7,27 +7,14 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-<<<<<<< d99a3f8558b315feb4b4af04d524e76a6c0a2158
-  this.route('home');
-  this.route('orgs');
-
-  this.route('org', function() {
-    this.route('emberjs', function() {
-      this.route('repos');
-      this.route('ember.js', function() {
-        this.route('issues');
-        this.route('contributors');
-      });
-=======
-  this.route('orgs', {}); // /orgs
   this.route('org', {path: 'org/:id'}, function() { 
     this.route('repos', {});
     this.route('repo', {path: ':id'}, function() {
       this.route('contributors');
       this.route('issues');
->>>>>>> set up basic router with templates
     });
   });
+  this.route('orgs');
 });
 
 export default Router;
