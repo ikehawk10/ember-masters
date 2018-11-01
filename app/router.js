d99a3f8558b315feb4b4af04d524e76a6c0a2158
Isaac <hawkins.isaac@gmail.com>
2018-11-01 14:59:18 -0500

    set up ember application

--- app/router.js
+++ app/router.js
@@ -7,6 +7,18 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('home');
+  this.route('orgs');
+
+  this.route('org', function() {
+    this.route('emberjs', function() {
+      this.route('repos');
+      this.route('ember.js', function() {
+        this.route('issues');
+        this.route('contributors');
+      });
+    });
+  });
 });
 
 export default Router;
